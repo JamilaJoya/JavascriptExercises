29bd9aa05a8ccdd297d16ae26a0f32b966cfe5d5
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-03-29 18:06:38 +0300

    Update objects-101.js

--- objects-101.js
+++ objects-101.js
@@ -13,3 +13,15 @@ myBook.title = `Animal Farm`; // CHANGE OBJECT PROPERTY VALUE
 console.log(`${myBook.title} by ${myBook.author}`);   // OUTPUT: Animal Farm by George Orwell
 
 
+// Lets have the second object of a person
+
+let person = {
+    name: 'Andrew',
+    age: 27,
+    location: 'Philadelphia'
+}
+console.log(`${person.name} is ${person.age} and lives in ${person.location}.`);
+// OUTPUT: Andrew is 27 and lives in Philadelphia.
+person.age = person.age + 1;
+console.log(`${person.name} is ${person.age} and lives in ${person.location}.`);
+// OUTPUT: Andrew is 28 and lives in Philadelphia.
