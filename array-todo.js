ce3f726cd0fbd063ffa31658712d66d4f8cad46c
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-03-31 18:14:46 +0300

    Create array-todo.js

---
+++ array-todo.js
@@ -0,0 +1,12 @@
+const todos = ['Buy food', 'Order cat food', 'Do the work', 'Clean Ketchin', 'Exercices'];
+
+console.log(`You have ${todos.length} todos.`);
+console.log(`Todo: ${todos[0]}`);
+console.log(`Todo: ${todos[todos.length - 2]}`);
+
+/*
+OUTPUT:
+You have 5 todos.
+Todo: Buy food
+Todo: Clean Ketchin
+*/
