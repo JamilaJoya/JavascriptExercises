12e846b072e587707ee323955ea146bd293fac8a
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-04-04 19:00:55 +0300

    Update todos-app.js

--- JavascriptInBrowser/DOMMnaipulation02/todos-app/todos-app.js
+++ JavascriptInBrowser/DOMMnaipulation02/todos-app/todos-app.js
@@ -1,8 +1,15 @@
 const paragraphs = document.querySelectorAll('p')
 
+// Filter Elements and remove them
 paragraphs.forEach(function (paragraph) {
     if (paragraph.textContent.toLowerCase().includes('the')) {
         paragraph.remove()
     }
 
 })
+
+
+// Adding new h1 element to html
+const newHeader = document.createElement('h1') // Create
+newHeader.textContent = ' this is the new header from javascript' // update
+document.querySelector('body').appendChild(newHeader) // add
