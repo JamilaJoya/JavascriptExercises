5ebad3df5b46357f6eb6d5d1fd5aa7f16fea10cb
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-04-13 16:56:30 +0300

    Add files via upload

--- JavascriptInBrowser/RenderingFilteredData/notes-app/notes-functions.js
+++ JavascriptInBrowser/RenderingFilteredData/notes-app/notes-functions.js
@@ -2,11 +2,7 @@
 // Read existing notes from local storage
 const getSavedNotes = () => {
     const notesJSON = localStorage.getItem('notes')
-    if (notesJSON !== null) {
-        return JSON.parse(notesJSON)
-    } else {
-        return []
-    }
+    return notesJSON !== null ? JSON.parse(notesJSON) : []
 }
 
 // Save notes to localStorage
