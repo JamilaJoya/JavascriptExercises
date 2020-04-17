c10dfceec6f4a30230f382fd953e8206dd42c0d1
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-04-17 16:57:26 +0300

    Add files via upload

--- oop/person.js
+++ oop/person.js
@@ -1,5 +1,28 @@
 // Prototaypal Inheritance
 
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}. `
+        this.likes.forEach((like) => {
+            bio += `${this.firstName} likes ${like}. `
+        });
+
+        return bio
+    }
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
+}
+
+/* 
 const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
@@ -21,6 +44,7 @@ Person.prototype.setName = function (fullName) {
     this.firstName = names[0]
     this.lastName = names[1]
 }
+ */
 
 const me = new Person('Andrew', 'Mead', 27, ['teaching', 'biking'])
 me.setName('Alexis Turner')
