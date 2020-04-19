aea4fd4c44934cfcc7a7403efc7b9e5fc777b979
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-04-19 12:59:34 +0300

    Add files via upload

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,6 @@
+// HTTP (Hypertext Transfer Protocol)
+// Request - what do we want to do
+// Response - what was actually done
 
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
@@ -6,11 +9,50 @@ const game1 = new Hangman('car parts', 2)
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGues(guess)
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
 
 })
+
+getPuzzle('2').then((puzzle) => {
+    console.log(puzzle)
+}, (err) => {
+    console.log(`Error: ${err}`)
+})
+
+getCountry('FI').then((country) => {
+    console.log(country.name)
+}, (err) => {
+    console.log(`Error: ${err}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
