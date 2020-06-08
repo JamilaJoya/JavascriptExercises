fec202a02a5cea4e51094e0f3baef3c261c677ac
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-06-08 23:01:08 +0300

    Update array.js

--- Array/array.js
+++ Array/array.js
@@ -1,6 +1,17 @@
+// How we can add one array into another
 var list = ['tiger', 'cut', 'bear', 'dog']
 console.log(list.concat(['Bee', 'beer']))
 
 OUTPUT: 
 ["tiger", "cut", "bear", "dog", "Bee", "beer"]
-// How we can add one array into another
+
+
+// CONCAT TWO ARRAY AND THEN SORT IT
+var myList = ['tiger', 'cut', 'bear', 'dog']
+var myNewList = myList.concat(['monkey'])
+
+myNewList.sort()
+console.log(myNewList)
+
+OUTPUT:
+ ["bear", "cut", "dog", "monkey", "tiger"]
