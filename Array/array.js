9d780d76a89b31a4f25d8a1095e5d96193c0105b
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-06-08 22:34:42 +0300

    Create array.js

---
+++ Array/array.js
@@ -0,0 +1,6 @@
+var list = ['tiger', 'cut', 'bear', 'dog']
+console.log(list.concat(['Bee', 'beer']))
+
+OUTPUT: 
+["tiger", "cut", "bear", "dog", "Bee", "beer"]
+// How we can add one array into another
