4f79fc4236dca6628f0434427303ed3bd5cf8232
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2020-06-29 22:35:33 +0300

    Create javascriptTerminology.js

---
+++ javascriptTerminology.js
@@ -0,0 +1,35 @@
+// function declaration
+function myFunction() {
+
+}
+
+// function expression 
+var newFunction = function () {
+
+}
+
+// expression
+1 + 3;
+var a = 2;
+return true;
+
+// calling or invoking a function
+alert()
+myFunction(par1, par2)
+
+// assign a variable
+var a = true;
+
+// function via method
+function thisIsAFunction() {
+
+}
+
+var obj = {
+    thisIsAMethod: function () {
+
+    }
+}
+
+thisIsAFunction()
+obj.thisIsAMethod()
